4eb819c24e29a9326a3454add6ca8cfbef219ec1
xyderos <kosxyderos@gmail.com>
2020-11-17 16:16:40 +0100

    added headAPI

--- src/headAPI.h
+++ src/headAPI.h
@@ -1,6 +1,8 @@
 #include "header.h"
+#include <stdlib.h>
+#include <sys/mman.h>
 
-//API for the head_t
+// API for the head_t
 head_t*       nxt(const head_t *const);
 
 head_t*       prv(const head_t *const);
@@ -11,9 +13,7 @@ void          detach(head_t*, head_t*);
 
 void          insert(head_t *, head_t *);
 
-head_t*       split(head_t *, const sz, const sz);
-
-const sz      adapt(const sz);
+head_t*       split(head_t *, const sz, sz);
 
 head_t*       find(head_t*, const sz);
 
