48ea892b85e72b8c2d3b44665610bbaed5240df5
xyderos <kosxyderos@gmail.com>
2020-11-17 14:53:31 +0100

    moved struct around

--- src/helpers.h
+++ src/helpers.h
@@ -6,3 +6,12 @@ const u16 ALIGN = 8U;
 
 const sz ARENA = 64 * 1024;
 
+// pack the struct in 24 bytes (8 byte alignment = 3 rows in the memory for 64
+// bit arch)
+typedef struct __attribute__((packed)) _head {
+
+  u16 bfree, bsize, free, size;
+
+  struct _head *next, *previous;
+
+} head_t;
