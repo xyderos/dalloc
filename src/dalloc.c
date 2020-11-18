bb5055ff8d1d2161d9fa03dbfacb0ece20ad23ae
xyderos <kosxyderos@gmail.com>
2020-11-18 22:39:16 +0100

    used wrappers and implemented the API for the client

--- src/dalloc.c
+++ src/dalloc.c
@@ -1,1 +1,59 @@
 #include "dalloc.h"
+
+sz used = 24;
+
+head_t* freeList;
+
+//wrapper function for dalloc(size)
+void *__dalloc(const sz size, head_t* arena) {
+
+  if(!arena){
+
+    freeList = initialise(arena);
+
+    freeList->next = freeList->previous = freeList;
+
+  }
+
+  if(size < 0) return NULL;
+
+  sz adapted = adapt(size);
+
+  head_t* block = find(freeList, adapted, &used);
+
+  if(!block) return NULL;
+
+  return hide(block);
+
+}
+
+void *dalloc(const sz size) {
+
+  static head_t *arena =NULL;
+
+  return __dalloc(size, arena);
+
+}
+
+//wrapper function dfree(pointer)
+void __dfree(const void *const pointer) {
+
+  if(!pointer) return;
+
+  head_t* block =(head_t*) magic(pointer);
+
+  head_t* aft   = after(pointer);
+
+  block->free   = 1;
+
+  aft->bfree    = 1;
+
+  insert(freeList,merge(freeList,block,&used));
+
+}
+
+void dfree(const void* const pointer){
+
+  __dfree(pointer);
+
+}
--- src/dalloc.h
+++ src/dalloc.h
@@ -1,7 +1,5 @@
 #include "headAPI.h"
 
-void printMemory();
-
 void* dalloc(const sz);
 
 void dfree(const void* const);
